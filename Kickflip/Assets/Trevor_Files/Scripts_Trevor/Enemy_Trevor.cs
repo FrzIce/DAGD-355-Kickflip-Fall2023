3d6b4384db1df0c330e9de97a6081ac1c65115d2
Trevor Baylie <Trevor@baylie.net>
2023-10-18 21:44:49 -0400

    Fixed Trevor Scene, using as base scene

--- Kickflip/Assets/Trevor_Files/Scripts_Trevor/Enemy_Trevor.cs
+++ Kickflip/Assets/Trevor_Files/Scripts_Trevor/Enemy_Trevor.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Player player;
+    public Animator animator;
+    public GameObject deathParticle;
+
+    public AudioSource death;
 
     public bool isFlier;
     public int health;
@@ -16,6 +20,14 @@ public class Enemy : MonoBehaviour
     public float verticalMovement;
     public bool grounded;
     private bool isDead = false;
+    bool facingRight = true;
+    bool jumpPlatform1 = false;
+    bool jumpPlatform2 = false;
+    bool facingPlayer = false;
+    bool flip = false;
+
+    bool underPlatform = false;
+    string platform = "Null";
 
     public Animator flyAnime;
 
@@ -24,48 +36,405 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        animator = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Animator>();
 
         health = 2;
         rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDead)
-        {
-            verticalMovement -= Time.deltaTime;
-            horizontalMovement = 0;
-        }
-        print(health);
-        if (player.transform.position.x < transform.position.x)
-        {
-            horizontalMovement = speed * -1;
-        }
-        else if (player.transform.position.x > transform.position.x) 
-        {
-            horizontalMovement = speed;
-        }
-       
 
-        if (isFlier && !isDead)
+        if (health <= 0)
         {
-            if (player.transform.position.y > transform.position.y + 2f)
-            {
-                verticalMovement = flyForce;
-            }
-            if (player.transform.position.y == transform.position.y)
-            {
-                verticalMovement = 0;
-            }
-            if (player.transform.position.y < transform.position.y)
-            {
-                verticalMovement = -flyForce / 2;
-            }
-            
+            death.Play();
+            var instance = Instantiate(deathParticle, transform.position, transform.rotation);
+            //Destroy(instance.gameObject, 1.0f);
+            Destroy(gameObject);
         }
-        Debug.Log("player y: " + player.transform.position.y + " Flier y: " + transform.position.y);
-        rb.velocity = new Vector2(horizontalMovement, verticalMovement);
+
+        if (player.transform.position.x > transform.position.x && (facingRight))
+        {
+            facingPlayer = true;
+        }
+        else if (player.transform.position.x < transform.position.x && (!facingRight))
+        {
+            facingPlayer = true;
+        }
+        else
+        {
+            facingPlayer = false;
+        }
+
+        if (!isFlier)
+        {
+            if (player.platform == "Null")
+            {
+                if (player.transform.position.x < transform.position.x)
+                {
+                    horizontalMovement = speed * -1;
+                }
+                else if (player.transform.position.x > transform.position.x)
+                {
+                    horizontalMovement = speed;
+                }
+            }
+            else if (player.platform == "A")
+            {
+                if (transform.position.x < -7 && transform.position.x > -17 && transform.position.y <= -2)
+                {
+                    underPlatform = true;
+                }
+                if (underPlatform == true)
+                {
+                    if (transform.position.x <= -12)
+                    {
+                        horizontalMovement = speed * -1;
+                    }
+                    else if (transform.position.x > -12)
+                    {
+                        horizontalMovement = speed;
+                    }
+                }
+                else if (transform.position.x <= -12)
+                {
+                    horizontalMovement = speed;
+                }
+                else if (transform.position.x > -12)
+                {
+                    horizontalMovement = speed * -1;
+                }
+
+
+                if (jumpPlatform1 == true)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                    grounded = false;
+                    underPlatform = false;
+                    if (facingPlayer == false)
+                    {
+                        Flip();
+                    }
+                }
+
+            }
+            else if (player.platform == "B")
+            {
+                if (transform.position.x < 27 && transform.position.x > 17 && transform.position.y <= -2)
+                {
+                    underPlatform = true;
+                }
+                if (underPlatform == true)
+                {
+                    if (transform.position.x <= 22)
+                    {
+                        horizontalMovement = speed * -1;
+                    }
+                    else if (transform.position.x > 22)
+                    {
+                        horizontalMovement = speed;
+                    }
+                }
+                else if (transform.position.x <= 22)
+                {
+                    horizontalMovement = speed;
+                }
+                else if (transform.position.x > 22)
+                {
+                    horizontalMovement = speed * -1;
+                }
+
+                if (jumpPlatform1 == true)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                    grounded = false;
+                    underPlatform = false;
+                    if (facingPlayer == false)
+                    {
+                        Flip();
+                    }
+                }
+            }
+            else if (player.platform == "C")
+            {
+                if (transform.position.y < 0)
+                {
+                    if (transform.position.x < -7 && transform.position.x > -17 && transform.position.y <= -2)
+                    {
+                        underPlatform = true;
+                    }
+                    if (underPlatform == true)
+                    {
+                        if (transform.position.x <= -12)
+                        {
+                            horizontalMovement = speed * -1;
+                        }
+                        else if (transform.position.x > -12)
+                        {
+                            horizontalMovement = speed;
+                        }
+                    }
+                    else if (transform.position.x <= -12)
+                    {
+                        horizontalMovement = speed;
+                    }
+                    else if (transform.position.x > -12)
+                    {
+                        horizontalMovement = speed * -1;
+                    }
+
+
+                    if (jumpPlatform1 == true)
+                    {
+                        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                        grounded = false;
+                        underPlatform = false;
+                        if (facingPlayer == false)
+                        {
+                            Flip();
+                        }
+                    }
+                }
+                if (transform.position.x >= -17 && transform.position.x <= -7)
+                {
+                    if (flip == false)
+                    {
+                        if (transform.position.x < -13)
+                        {
+                            horizontalMovement = speed;
+                        }
+                        else if (transform.position.x > -11)
+                        {
+                            horizontalMovement = speed * -1;
+                        }
+                        else if (transform.position.x >= -13 && transform.position.x <= -11)
+                        {
+                            flip = true;
+                        }
+                    }
+                    else if (flip == true)
+                    {
+                        horizontalMovement = speed * -1;
+                        if (jumpPlatform2 == true)
+                        {
+                            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                            grounded = false;
+                            if (facingPlayer == false)
+                            {
+                                Flip();
+                            }
+                        }
+                    }
+
+                }
+            }
+            else if (player.platform == "D")
+            {
+                if (transform.position.x <= 6)
+                {
+                    if (transform.position.y < -.5)
+                    {
+
+                        if (transform.position.x < -7 && transform.position.x > -17 && transform.position.y <= -2)
+                        {
+                            underPlatform = true;
+                        }
+                        if (underPlatform == true)
+                        {
+                            if (transform.position.x <= -12)
+                            {
+                                horizontalMovement = speed * -1;
+                            }
+                            else if (transform.position.x > -12)
+                            {
+                                horizontalMovement = speed;
+                            }
+                        }
+                        else if (transform.position.x <= -12)
+                        {
+                            horizontalMovement = speed;
+                        }
+                        else if (transform.position.x > -12)
+                        {
+                            horizontalMovement = speed * -1;
+                        }
+
+
+                        if (jumpPlatform1 == true)
+                        {
+                            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                            grounded = false;
+                            underPlatform = false;
+                            if (facingPlayer == false)
+                            {
+                                Flip();
+                            }
+                        }
+                    }
+                    else if (transform.position.x >= -17 && transform.position.x <= -7)
+                    {
+                        if (flip == false)
+                        {
+                            if (transform.position.x < -13)
+                            {
+                                horizontalMovement = speed;
+                            }
+                            else if (transform.position.x > -11)
+                            {
+                                horizontalMovement = speed * -1;
+                            }
+                            else if (transform.position.x >= -13 && transform.position.x <= -11)
+                            {
+                                flip = true;
+                            }
+                        }
+                        else if (flip == true)
+                        {
+                            print("test");
+                            horizontalMovement = speed;
+                            if (jumpPlatform2 == true)
+                            {
+                                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                                grounded = false;
+                                if (facingPlayer == false)
+                                {
+                                    Flip();
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+
+                    }
+                }
+                else if (transform.position.x > 6) //enemy moves right not left
+                {
+                    if (transform.position.y < -.5)
+                    {
+
+                        if (transform.position.x < 27 && transform.position.x > 17 && transform.position.y <= -2)
+                        {
+                            underPlatform = true;
+                        }
+                        if (underPlatform == true)
+                        {
+                            if (transform.position.x <= 22)
+                            {
+                                horizontalMovement = speed * -1;
+                            }
+                            else if (transform.position.x > 22)
+                            {
+                                horizontalMovement = speed;
+                            }
+                        }
+                        else if (transform.position.x <= 22)
+                        {
+                            horizontalMovement = speed;
+                        }
+                        else if (transform.position.x > 22)
+                        {
+                            horizontalMovement = speed * -1;
+                        }
+
+
+                        if (jumpPlatform1 == true)
+                        {
+                            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                            grounded = false;
+                            underPlatform = false;
+                            if (facingPlayer == false)
+                            {
+                                Flip();
+                            }
+                        }
+                    }
+                    else if (transform.position.x >= 17 && transform.position.x <= 27)
+                    {
+                        if (flip == false)
+                        {
+                            if (transform.position.x < 21)
+                            {
+                                horizontalMovement = speed;
+                            }
+                            else if (transform.position.x > 23)
+                            {
+                                horizontalMovement = speed * -1;
+                            }
+                            else if (transform.position.x >= 21 && transform.position.x <= 23)
+                            {
+                                flip = true;
+                            }
+                        }
+                        else if (flip == true)
+                        {
+                            print("test");
+                            horizontalMovement = speed * -1;
+                            if (jumpPlatform2 == true)
+                            {
+                                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                                grounded = false;
+                                if (facingPlayer == false)
+                                {
+                                    Flip();
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        //if (isFlier) //Alex Code
+        //{
+        //    if (player.transform.position.y > transform.position.y + 2f)
+        //    {
+        //        verticalMovement = flyForce;
+        //    }
+        //    if (player.transform.position.y == transform.position.y)
+        //    {
+        //        verticalMovement = 0;
+        //    }
+        //    if (player.transform.position.y < transform.position.y)
+        //    {
+        //        verticalMovement = -flyForce / 2;
+        //    }
+
+        //}
+        //Debug.Log("player y: " + player.transform.position.y + " Flier y: " + transform.position.y);
+        //rb.velocity = new Vector2(horizontalMovement, verticalMovement);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement)); // animation
+
+        if (horizontalMovement > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (horizontalMovement < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+
+        jumpPlatform1 = false;
+        jumpPlatform2 = false;
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -73,11 +442,58 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.tag == "Recall Collision")
         {
             health -= 2;
+
         }
-        else if(collision.gameObject.tag == "Jump Platform")
+        else if (collision.gameObject.tag == "Jump Platform")
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            grounded = false;
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //grounded = false;
+            jumpPlatform1 = true;
+        }
+        else if (collision.gameObject.tag == "Jump Platform 2")
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //grounded = false;
+            print("can jump");
+            jumpPlatform2 = true;
+        }        
+        else if (collision.gameObject.tag == "Platform")
+        {
+            print("Colliding");
+            if (transform.position.y <= 6) // Level 1
+            {
+                if (transform.position.x < 0) //A
+                {
+                    platform = "A";
+                }
+                else if (transform.position.x > 0) //B
+                {
+                    platform = "B";
+                }
+            }
+            else if (transform.position.y <= 11 && transform.position.y > 6) // Level 2
+            {
+                if (transform.position.x < -10) //C
+                {
+                    platform = "C";
+                }
+                else if (transform.position.x > -10) //D
+                {
+                    platform = "D";
+                }
+            }
+            else if (transform.position.y > 11) // Level 3
+            {
+                if (transform.position.x < 10) //E
+                {
+                    platform = "E";
+                }
+                else if (transform.position.x > 10) //F
+                {
+                    platform = "F";
+                }
+
+            }
         }
         if (collision.gameObject.tag == "Bullet")
         {
@@ -91,12 +507,15 @@ public class Enemy : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            death.Play();
+            var instance = Instantiate(deathParticle, transform.position, transform.rotation);
+            //Destroy(instance.gameObject, 1.0f);
             Destroy(gameObject);
+
         }
-        
-        if (collision.gameObject.tag == "Platform" && isFlier)
+        else if (collision.gameObject.tag == "Floor")
         {
-            horizontalMovement = player.transform.position.x * flyForce;
+            platform = "Null";
         }
     }
 
